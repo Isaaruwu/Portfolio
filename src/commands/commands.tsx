ea03915d43fe7d394f21c9a3d8708ac0f1480f20
Lia Fox <ismailaarabb@gmail.com.com>
2023-07-08 17:37:05 -0400

    ✨ Initial Commit

---
+++ src/commands/commands.tsx
@@ -0,0 +1,121 @@
+import React from 'react'
+import styles from './commands.module.scss'
+import { links, info } from '../config'
+import { Commands, Command } from '../typings'
+import ListElement from '../ListElement/ListElement'
+
+const rawCommands: Command[] = [
+	{
+		name: 'help',
+		icon: 'fas fa-fw fa-question-circle',
+		description: 'List available commands',
+		execute(app) {
+			const { commands } = app.state
+			return (
+				<>
+					Available commands:
+					{[...commands.values()].map(
+						({ icon, name, description }, key) => (
+							<ListElement
+								key={key}
+								icon={icon}
+								name={name}
+								description={description}
+								help
+							/>
+						)
+					)}
+				</>
+			)
+		},
+	},
+	{
+		name: 'about',
+		icon: 'fas fa-fw fa-info-circle',
+		description: 'Show about me',
+		execute(app) {
+			const { userDataLoaded, userData } = app.state
+			if (!userDataLoaded)
+				return <>User data could not be fetched</>
+			const { avatar_url, login, name, bio } = userData
+			return (
+				<div className={styles.infoWrapper}>
+					<div className={styles.infoInner}>
+						<img
+							src={avatar_url}
+							className={styles.avatar}
+							alt="GitHub avatar"
+						/>
+						<div className={styles.content}>
+							<div className={styles.header}>
+								<strong>{name}</strong>{' '}
+								<span className="muted">@{login}</span>
+							</div>
+							<em className={styles.bio}>"...{bio}"</em>
+							<div className={styles.info}>{info} </div>
+						</div>
+					</div>
+				</div>
+			)
+		},
+	},
+	{
+		name: 'projects',
+		icon: 'fas fa-fw fa-tools',
+		description: 'List of my major projects',
+		execute(app) {
+			const { projectDataLoaded, projectData } = app.state
+			if (!projectDataLoaded)
+				return <>Project data could not be fetched</>
+			return (
+				<>
+					{projectData.map(
+						({ name, html_url, description }: any, key: number) => (
+							<ListElement
+								key={key}
+								icon={'fab fa-fw fa-github-alt'}
+								name={name}
+								link={html_url}
+								description={description}
+							/>
+						)
+					)}
+				</>
+			)
+		},
+	},
+	{
+		name: 'links',
+		icon: 'fas fa-fw fa-link',
+		description: 'Display social links',
+		execute() {
+			return (
+				<>
+					{links.map(({ icon, name, link, description }, key) => (
+						<ListElement
+							key={key}
+							icon={icon}
+							name={name}
+							link={link}
+							description={description}
+						/>
+					))}
+				</>
+			)
+		},
+	},
+	{
+		name: 'clear',
+		icon: 'fas fa-fw fa-eraser',
+		description: 'Clear terminal screen',
+		execute(app) {
+			app.setState({
+				...app.state,
+				record: [],
+			})
+		},
+	},
+]
+const commands: Commands = new Map(rawCommands.map(cmd => [cmd.name, cmd]))
+
+export default commands
---
+++ src/config.tsx
@@ -0,0 +1,72 @@
+import React from 'react'
+const github_username = 'Isaaruwu'
+const discord_usertag = 'Isaar#9003'
+const discord_userid = '795096902591971338'
+const email = 'ismailaarabb@gmail.com'
+
+const projects = [
+	'Isaaruwu/Plant-e',
+	// 'Isaaruwu/EstateGuardian', private
+	'Isaaruwu/TomStock',
+	'Isaaruwu/Template-Generator',
+	// 'Isaaruwu/StockAI2', private repo
+	'Isaaruwu/SmollBot',
+]
+
+const links = [
+	{
+		name: 'Linkedin',
+		icon: 'fab fa-fw fa-linkedin-in',
+		link: 'https://www.linkedin.com/in/ismail-aarab-25b2061b0/',
+		description: 'Connect with me!',
+	},
+	{
+		name: 'GitHub',
+		icon: 'fab fa-fw fa-github',
+		link: 'https://github.com/Isaaruwu',
+		description: 'My Github profile',
+	},
+	{
+		name: 'E-mail',
+		icon: 'fas fa-fw fa-at',
+		link: `mailto:${email}`,
+		description: 'Lets get in touch!',
+	}
+]
+
+const info = (
+	<>
+		<div style={{ lineHeight: '30px' }}>
+			<br></br>
+			<p>Hi!👋</p><br></br>
+			<p>
+				I'm <span className="highlighted">Ismail Aarab</span>, currently a{' '}
+				student at <span className="highlighted">Polytechnique Montréal</span> in {' '}
+				Software Engineering :)
+			</p>
+			<p>
+				I like to write various scripts to automate my daily tasks and I've been
+				doing some{' '}
+				<span className="highlighted">Data Engineering & AI</span> in school and 
+				for fun on my spare time.
+				I also like to play around with{' '}
+				<span className="highlighted">building full stack web applications</span>!
+			</p><br></br>
+			<p>
+				Here's a list of some of the technologies I've been working with recently 
+				(it's only from my public repos, so it's not exhaustive 😉 ):
+			</p> <br></br>
+			<img alt="stats" src="https://github-readme-stats.vercel.app/api/top-langs/?username=isaaruwu&show_icons=true&theme=dark&bg_color=0A0A0A&locale=en"/>
+		</div>
+	</>
+)
+
+export {
+	github_username,
+	discord_usertag,
+	discord_userid,
+	email,
+	projects,
+	links,
+	info,
+}
